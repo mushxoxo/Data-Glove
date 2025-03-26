ef51276c5e5327328d089c6809148efb5f0f4d1a
mushxoxo <bhanuagrawal12@gmail.com>
2025-03-26 12:18:06 +0530

    Added img, img_path.csv, label_count.json configurations

--- Database/log.py
+++ Database/log.py
@@ -1,11 +1,32 @@
 import serial
 import time
 import csv
+import json
 
-ser = serial.Serial('COM7', 115200, timeout=1)  # Use the correct port
-time.sleep(2)  # Allow time for serial connection
+port = "/dev/ttyUSB0"
+baudrate = 115200
 
-filename = "mpu_data.csv"
+ser = serial.Serial(port, baudrate, timeout=1)  # Use the correct port
+time.sleep(2)                                   # Allow time for serial connection
+
+
+
+
+try:
+    with open("label_count.json", "r") as f:
+        label_count = json.load(f)                     # Convert JSON to dictionary
+except (FileNotFoundError, json.JSONDecodeError):
+    label_count = {}                                   # Initialize if not found
+
+label_count_copy = label_count.copy()
+
+#while 1:
+
+label = input("Enter label: ")
+label_count_copy[label] = label_count_copy.get(label, -1) + 1
+
+
+filename = "img/"+str(label)+"_"+str(label_count_copy[label])
 with open(filename, "w", newline="") as file:
     writer = csv.writer(file)
     writer.writerow(["A_0", "A_1", "A_2", "G_0", "G_1", "G_2"])  # Column headers
@@ -18,6 +39,23 @@ with open(filename, "w", newline="") as file:
             if len(data) == 6:
                 writer.writerow(map(int, data))
 
-print(f"Data saved in {filename}")
+with open("img_path.csv", "w", newline="") as f:
+    writer = csv.writer(f)
+    writer.writerow([str(label) + "_" + str(label_count_copy[label]), filename])
+
+with open("label_count.json", "w+") as f:
+    json.dump(label_count_copy, f, indent = 4)
+
+
+
+
+
+
+
+
+
+
+
+
 ser.close()
 
